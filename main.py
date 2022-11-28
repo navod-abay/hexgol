336a3d7eb83ba88ca4b8706edbdd259f48e19650
Emil Navod <Emil.navod@gmail.com>
2022-11-28 14:40:55 +0530

    game of life - hexagons

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import tkinter as tk
+import math
 
-WIDTH = 10 # number of tiles in a row
-HEIGHT = 10 # number of rows in the honeycomb (both odd and even)
-RADIUS = 20 # the length between a tile center and an edge
-DISTANCE = 25 # the length between two tile centers
+WIDTH = 10  # number of tiles in a row
+HEIGHT = 10  # number of rows in the honeycomb (both odd and even)
+R = 20  # Radias: the length between a tile center and an edge
+RSec
+DISTANCE = 25  # the length between two tile centers
+SEC30 = 1 / math.cos(math.pi / 6)
 
 
 class Node:
     def __init__(self, u_id, row, pos):
-        assert (u_id == row * WIDTH + pos)
         self.id = u_id
         self.row = row
         self.pos = pos
         self.state = False
         self.neighbours = self.find_neighbours()
-
-    def draw_hexagon(self, r, p):
-        if (r % 2 == 1) :
-            X = (2 * p - 1) * DISTANCE /2
-        else:
-            X = p * DISTANCE
 
     def find_neighbours(self):
         neighbours = [(self.row, self.pos - 1), (self.row, self.pos + 1), (self.row + 1, self.pos - (self.row % 2)),
@@ -34,9 +30,23 @@ class Board(tk.Canvas):
         super(Board, self).__init__(root)
         self.tiles = self.initiate_tiles()
 
-    @staticmethod
-    def initiate_tiles():
-        return []
+    def initiate_tiles(self):
+        tiles = []
+        for row in range(1, HEIGHT + 1):
+            for position in range(WIDTH + 1):
+                unique_id = self.draw_hexagon(row, position)
+                tile = Node(unique_id, row, position)
+                tiles.append(tile)
+        return tiles
+
+    def draw_hexagon(self, r, p):
+        if r % 2 == 1:  # calculate the center of the tile
+            x = (2 * p - 1) * DISTANCE / 2
+            y = (3 * r - 2) * DISTANCE * SEC30 / 2
+        else:
+            x = p * DISTANCE
+            y = (3 * r - 1) * DISTANCE * SEC30 / 4
+        tag = self.create_polygon(x - R, y +)
 
 
 root = tk.Tk()
